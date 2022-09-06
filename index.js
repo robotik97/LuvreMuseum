e281c9b2767319a48bee43e1d5eed4eb45629c46
robotik97 <edhar97@gmail.com>
2022-09-06 23:02:17 +0300

    fix: change size photo Mono

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const progress = document.querySelector('.video-progress');
+// const progress = document.querySelector('.video-progress');
   
-progress.addEventListener('input', function() {
-  const value = this.value;
-  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, white 100%)`
-})
+// progress.addEventListener('input', function() {
+//   const value = this.value;
+//   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, white 100%)`
+// })const navMenu = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const links = document.querySelector(".links");
+    const navLinks = document.querySelectorAll(".links Li");
+
+// Toggle nav
+    hamburger.onclick = function () {
+        links.classList.toggle('link-active');
+//Animate links
+        navLinks.forEach((link, index) =>{
+        if (link.style.animation) {
+            link.style.animation = ''
+        } else {
+            link.style.animation = `hamburgerFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+        });
+        //Hamburger animationn
+    hamburger.classList.toggle('toggle');
+    };
+
+navMenu();
