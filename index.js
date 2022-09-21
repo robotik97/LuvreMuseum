bf3327817d008fb42f9e0a16826a55b8860033f2
robotik97 <edhar97@gmail.com>
2022-09-21 21:29:04 +0300

    feat: add burger menu

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-// const progress = document.querySelector('.video-progress');
-  
-// progress.addEventListener('input', function() {
-//   const value = this.value;
-//   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, white 100%)`
-// })const navMenu = () => {
-    const hamburger = document.querySelector(".hamburger");
-    const links = document.querySelector(".links");
-    const navLinks = document.querySelectorAll(".links Li");
+const navMenu = () => {
+  const hamburger = document.querySelector(".hamburger");
+  const links = document.querySelector(".links");
+  const navLinks = document.querySelectorAll(".links Li");
 
-// Toggle nav
-    hamburger.onclick = function () {
-        links.classList.toggle('link-active');
-//Animate links
-        navLinks.forEach((link, index) =>{
-        if (link.style.animation) {
-            link.style.animation = ''
-        } else {
-            link.style.animation = `hamburgerFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-        }
-        });
-        //Hamburger animationn
-    hamburger.classList.toggle('toggle');
-    };
-
+  // Toggle nav
+  hamburger.onclick = function () {
+    links.classList.toggle("link-active");
+    //Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `hamburgerFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    //Hamburger animationn
+    hamburger.classList.toggle("toggle");
+  };
+};
 navMenu();
